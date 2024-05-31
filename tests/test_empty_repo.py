f7c3ad40fe881552bd0b3ded21a97ff5b802863a
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2024-05-31 10:43:02 +0200

    add todo

--- tests/test_empty_repo.py
+++ tests/test_empty_repo.py
@@ -11,6 +11,7 @@ def test_running_inside_an_empty_git_repository(empty_git_repo: Path) -> None:
         cwd=empty_git_repo,
     )
     assert result.returncode == 0
+    # TODO: ensure empty stderr
 
     git_log = get_git_log(empty_git_repo)
     assert len(git_log) == 1
